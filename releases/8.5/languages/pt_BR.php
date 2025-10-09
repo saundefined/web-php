4673633219eab84edb36abf9da33e8570438d893
Marcos Marcolin <marcolindev@gmail.com>
2025-10-09 13:09:16 -0300

    docs: add PHP 8.5 translation to pt_BR.

--- releases/8.5/languages/pt_BR.php
+++ releases/8.5/languages/pt_BR.php
@@ -1,27 +1,27 @@
 <?php
 
 return [
-    'common_header' => 'PHP 8.5 is a major update of the PHP language. It contains many new features, such as the new URI extension, support for modifying properties while cloning, the Pipe operator, performance improvements, bug fixes, and general cleanup.',
-    'documentation' => 'Doc',
-    'main_title' => 'Released!',
-    'main_subtitle' => 'PHP 8.5 is a major update of the PHP language.<br class="display-none-md"> It contains many new features, such as the new URI extension, support for modifying properties while cloning, the Pipe operator, performance improvements, bug fixes, and general cleanup.',
-    'upgrade_now' => 'Upgrade to PHP 8.5 now!',
+    'common_header' => 'O PHP 8.5 é uma atualização importante da linguagem PHP. Ela traz diversos novos recursos, como a nova extensão URI, suporte para modificar propriedades durante a clonagem de objetos, o operador Pipe, além de melhorias de desempenho, correções de bugs e uma limpeza geral no código da linguagem.',
+    'documentation' => 'Documentação',
+    'main_title' => 'Lançado!',
+    'main_subtitle' => 'PHP 8.5 é uma atualização importante da linguagem PHP.<br class="display-none-md"> Ela traz diversos novos recursos, como a nova extensão URI, suporte para modificar propriedades durante a clonagem de objetos, o operador Pipe, além de melhorias de desempenho, correções de bugs e uma limpeza geral na base da linguagem.',
+    'upgrade_now' => 'Atualize para PHP 8.5 agora!',
 
-    'pipe_operator_title' => 'Pipe operator',
-    'url_parsing_api_title' => 'Add RFC 3986 and WHATWG URL compliant API',
+    'pipe_operator_title' => 'Operador Pipe',
+    'url_parsing_api_title' => 'API compatível com RFC 3986 e WHATWG para análise de URLs',
     'url_parsing_api_description' => '<code>Uri\UriException</code>, <code>Uri\InvalidUriException</code>, <code>Uri\UriComparisonMode</code>,
-    <code>Uri\Rfc3986\Uri</code>, <code>Uri\WhatWg\InvalidUrlException</code>, <code>Uri\WhatWg\UrlValidationErrorType</code>, <code>Uri\WhatWg\UrlValidationError</code>, and <code>Uri\WhatWg\Url</code> are now available.',
+    <code>Uri\Rfc3986\Uri</code>, <code>Uri\WhatWg\InvalidUrlException</code>, <code>Uri\WhatWg\UrlValidationErrorType</code>, <code>Uri\WhatWg\UrlValidationError</code>, e <code>Uri\WhatWg\Url</code> estão disponíveis.',
     'clone_with_title' => 'Clone with v2',
-    'no_discard_title' => 'New <code>#[\NoDiscard]</code> Attribute',
-    'fcc_in_const_expr_title' => 'First Class Callables in constant expressions',
-    'curl_share_persistence_improvement_title' => 'Persistent cURL share handle improvement',
-    'array_first_last_title' => 'New <code>array_first()</code> and <code>array_last()</code> functions',
+    'no_discard_title' => 'Novo atributo <code>#[\NoDiscard]</code>',
+    'fcc_in_const_expr_title' => 'Callables de primeira classe em expressões constantes',
+    'curl_share_persistence_improvement_title' => 'Melhoria na persistência do manipulador compartilhado do cURL',
+    'array_first_last_title' => 'Novas funções <code>array_first()</code> e <code>array_last()</code>',
 
-    'new_classes_title' => 'New Classes, Interfaces, and Functions',
+    'new_classes_title' => 'Novas classes, interfaces e funções',
 
-    'bc_title' => 'Deprecations and backward compatibility breaks',
+    'bc_title' => 'Deprecações e quebras de compatibilidade retroativa',
 
-    'footer_title' => 'Better performance, better syntax, improved type safety.',
-    'footer_description' => '<p>For source downloads of PHP 8.5 please visit the <a href="/downloads">downloads</a> page. Windows binaries can be found on the <a href="https://windows.php.net/download">PHP for Windows</a> site. The list of changes is recorded in the <a href="/ChangeLog-8.php#PHP_8_5">ChangeLog</a>.</p>
-        <p>The <a href="/manual/en/migration85.php">migration guide</a> is available in the PHP Manual. Please consult it for a detailed list of new features and backward-incompatible changes.</p>',
+    'footer_title' => 'Melhor desempenho, sintaxe aprimorada e maior segurança de tipos.',
+    'footer_description' => '<p>Para baixar o código-fonte do PHP 8.5, visite a página de <a href="/downloads">downloads</a>. Os binários para Windows podem ser encontrados no site <a href="https://windows.php.net/download">PHP for Windows</a>. A lista de alterações está registrada no <a href="/ChangeLog-8.php#PHP_8_5">ChangeLog</a>.</p>
+        <p>O <a href="/manual/pt_BR/migration85.php">guia de migração</a> está disponível no Manual do PHP. Consulte-o para uma lista detalhada de novos recursos e mudanças incompatíveis com versões anteriores.</p>',
 ];
